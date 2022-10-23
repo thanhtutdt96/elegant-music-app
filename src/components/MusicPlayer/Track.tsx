b9122fb588e684331193d0b24939b3ac516da307
Tu Pham <thanhtutdt96@gmail.com>
2022-10-23 17:34:51 +0700

    Update styling

--- src/components/MusicPlayer/Track.tsx
+++ src/components/MusicPlayer/Track.tsx
@@ -8,14 +8,14 @@ type Props = {
 };
 
 const Track: React.FC<Props> = ({ isPlaying, isActive, activeSong }) => (
-  <div className="flex-1 flex items-center justify-start">
+  <div className="flex-1 flex items-center justify-start w-[50%]">
     <div
       className={`${
         isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''
       } hidden sm:block h-16 w-16 mr-4`}>
       <img src={activeSong?.images?.coverart} alt="cover art" className="rounded-full" />
     </div>
-    <div className="w-[50%]">
+    <div className="flex-1 truncate">
       <p className="truncate text-white font-bold text-lg">
         {activeSong?.title ? activeSong?.title : 'No active Song'}
       </p>
--- src/components/SongCard/index.tsx
+++ src/components/SongCard/index.tsx
@@ -28,8 +28,8 @@ const SongCard: React.FC<Props> = ({ song, index, isPlaying, activeSong, data })
   };
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <div className="relative w-full h-56 group">
+    <div className="flex flex-col w-[150px] sm:w-[220px] lg:w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full group">
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex
         ${activeSong?.key === song.key ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
