a860265a2872b99dd8dc9c7b8c9e2d13a2a47a4e
Tu Pham <thanhtutdt96@gmail.com>
2022-10-23 17:59:36 +0700

    Update styling

--- src/components/MusicPlayer/index.tsx
+++ src/components/MusicPlayer/index.tsx
@@ -58,7 +58,7 @@ const MusicPlayer = () => {
   return (
     <div className="relative sm:px-12 px-8 w-full flex items-center justify-between">
       <Track isPlaying={isPlaying} isActive={isActive} activeSong={activeSong} />
-      <div className="flex-1 flex flex-col items-center justify-center">
+      <div className="flex-1 flex flex-col items-end md:items-center justify-center">
         <Controls
           isPlaying={isPlaying}
           isRepeat={isRepeat}
