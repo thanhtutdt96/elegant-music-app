79e4ed816de936e06c4232f96ebc7054683fed19
Tu Pham <thanhtutdt96@gmail.com>
2022-10-23 17:46:19 +0700

    Update styling

--- src/components/ArtistCard/index.tsx
+++ src/components/ArtistCard/index.tsx
@@ -11,11 +11,11 @@ const ArtistCard: React.FC<Props> = ({ track }) => {
 
   return (
     <div
-      className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80
+      className="flex flex-col w-[150px] sm:w-[220px] lg:w-[250px] p-4 bg-white/5 bg-opacity-80
         backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
       role="presentation"
       onClick={() => navigate(`/artists/${track?.artists?.[0]?.adamid}`)}>
-      <img src={track?.images?.coverart} alt="artist" className="w-full h-56 rounded-lg" />
+      <img src={track?.images?.coverart} alt="artist" className="w-full h-auto rounded-lg" />
       <p className="mt-4 font-semibold text-lg truncate text-white">{track?.subtitle}</p>
     </div>
   );
