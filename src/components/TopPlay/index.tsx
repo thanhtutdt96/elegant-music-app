a2b18908595331d8aed71266584c99e7b42fedf0
Tu Pham <thanhtutdt96@gmail.com>
2022-10-23 18:19:27 +0700

    Update scroll behaviour

--- src/components/TopPlay/index.tsx
+++ src/components/TopPlay/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'redux/hooks';
 import { useGetTopChartsQuery } from 'redux/services/shazamCore';
