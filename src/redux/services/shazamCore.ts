fba18fd1b3c3ff159709340eaf6f0914d6863b62
Tu Pham <thanhtutdt96@gmail.com>
2023-06-19 22:44:15 +0700

    Update shazamCore api

--- src/redux/services/shazamCore.ts
+++ src/redux/services/shazamCore.ts
@@ -8,7 +8,7 @@ import { SongDetail } from 'types/SongDetail';
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: 'https://shazam-core.p.rapidapi.com',
     prepareHeaders: (headers) => {
       headers.set('X-RapidAPI-Key', RAPID_API_KEY);
 
@@ -16,24 +16,24 @@ export const shazamCoreApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query<Song[], void>({ query: () => '/charts/world' }),
+    getTopCharts: builder.query<Song[], void>({ query: () => 'v1/charts/world' }),
     getSongDetails: builder.query<SongDetail, string>({
-      query: (songId: string) => `/tracks/details?track_id=${songId}`,
+      query: (songId: string) => `/v1/tracks/details?track_id=${songId}`,
     }),
     getRelatedSongs: builder.query<Song[], string>({
-      query: (songId: string) => `/tracks/related?track_id=${songId}`,
+      query: (songId: string) => `/v1/tracks/related?track_id=${songId}`,
     }),
     getArtistDetails: builder.query<ArtistDetail, string>({
-      query: (artistId: string) => `/artists/details?artist_id=${artistId}`,
+      query: (artistId: string) => `/v2/artists/details?artist_id=${artistId}`,
     }),
     getSongsByCountry: builder.query<Song[], string>({
-      query: (countryCode: string) => `/charts/country?country_code=${countryCode}`,
+      query: (countryCode: string) => `/v1/charts/country?country_code=${countryCode}`,
     }),
     getSongsByGenre: builder.query<Song[], string>({
-      query: (genre: string) => `/charts/genre-world?genre_code=${genre}`,
+      query: (genre: string) => `/v1/charts/genre-world?genre_code=${genre}`,
     }),
     getSongsBySearch: builder.query<SearchResult, string>({
-      query: (searchTerm: string) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+      query: (searchTerm: string) => `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
     }),
   }),
 });
